875e489300eaa0ed5accd82124989087f31b8b53
lamak-shahiwala <shahiwalalamak@gmail.com>
2025-12-12 23:37:38 +0530

    Buttons got hover effect

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
 type CTAButtonProps = {
   children: React.ReactNode;
@@ -10,13 +10,13 @@ type CTAButtonProps = {
 export default function CTAButton({
   children,
   onClick,
-  className = '',
+  className = "",
 }: CTAButtonProps) {
   return (
     <button
       onClick={onClick}
-      aria-label={'Trade'}
-      className={`inline-flex items-center justify-center px-8 py-3 rounded-full font-semibold shadow-sm transition-shadow focus:outline-none focus:ring-2 focus:ring-offset-1 bg-primary text-white font-body ${className}`}
+      aria-label={"Trade"}
+      className={`inline-flex items-center justify-center px-8 py-3 rounded-full font-semibold shadow-sm transition-shadow focus:outline-none focus:ring-2 focus:ring-offset-1 bg-primary text-white font-body hover:bg-[#28a84d] ${className}`}
     >
       {children}
     </button>
--- components/CreateButton.tsx
+++ components/CreateButton.tsx
@@ -1,14 +1,8 @@
-
-
-export default function CreateButton(){
-    return (
-        <div className="flex justify-center items-center px-4 py-2 border-2 border-text bg-text text-bg rounded-full h-[8vh]">
-            <button className="hidden md:block">
-                + Create
-            </button>
-            <button className="block md:hidden">
-                Create
-            </button>
-        </div>
-    )
+export default function CreateButton() {
+  return (
+    <div className="flex justify-center items-center px-4 py-2 border border-text bg-text text-bg rounded-full h-[8vh] hover:border-black/85 hover:bg-black/85 hover:cursor-pointer">
+      <button className="hidden md:block">+ Create</button>
+      <button className="block md:hidden">Create</button>
+    </div>
+  );
 }
